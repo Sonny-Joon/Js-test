562abb8e895d68d7c89f144106b3bde0825aad4c
Sonny-Joon <sonnyjoon@yandex.by>
2020-12-08 20:20:06 +0300

    first test

---
+++ test.js
@@ -0,0 +1,6 @@
+require('chromedriver');
+var webdriver = require('selenium-webdriver');
+var driver = new webdriver.Builder()
+  .forBrowser('chrome')
+  .build();
+driver.get('https://google.com');
