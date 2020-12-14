ef14940731e562116ae293b4682d68c12548cb7c
Sonny-Joon <sonnyjoon@yandex.by>
2020-12-14 20:44:49 +0300

    test assert example

---
+++ homework/assert._test.js
@@ -0,0 +1,7 @@
+describe('Negative Expectaion Demo', () => {
+    it('Negative Expectaion Example', () => {
+        let a = 10;
+        let b = 20;
+        expect(a).not.toBe(b); //not is chained to expect
+    });
+})
