ca707b91889d233ddeb7417ea331d69d65bf1ad6
Sonny-Joon <sonnyjoon@yandex.by>
2021-01-20 13:39:23 +0300

    revert navig_test

--- tests/navigation_test.ts
+++ tests/navigation_test.ts
@@ -1,67 +1,81 @@
 import { browser, by, element, protractor } from "protractor";
 import { HomePage } from "../pages/homepage";
 import {URL} from '../utils/constants';
-import { Functions } from "../pages/functions";
-
 let addHomePage = new HomePage ();
-let addFunctions = new Functions ();
 
-describe('Yandex5', () => {
-    it('Click login button', async () => {
-    await browser.get(URL);
-    await addHomePage.ClickOnVideoButton ();
-    await addFunctions.openNewTab ();
-    await browser.getCurrentUrl().then(function(url) {
-    console.log("URL= "+ url);
-    expect(url).toContain("https://yandex.by/video"); 
-    })
-    await addFunctions.goBackToPreviousTab ();
-    await addHomePage.ClickOnImagesButton ();
-    await addFunctions.openNewTab ();
-    await browser.getCurrentUrl().then(function(url) {
-    console.log("URL= "+ url);
-    expect(url).toContain("https://yandex.by/images"); 
-    })
-    await addFunctions.goBackToPreviousTab ();
-    await addHomePage.ClickOnNewsButton ();
-    await addFunctions.openNewTab ();
-    await browser.getCurrentUrl().then(function(url) {
-    console.log("URL= "+ url);
-    expect(url).toContain("https://yandex.by/news"); 
-    })
-    await addFunctions.goBackToPreviousTab ();
-    await addHomePage.ClickOnMapsButton ();
-    await addFunctions.openNewTab ();
-    await browser.getCurrentUrl().then(function(url) {
-    console.log("URL= "+ url);
-    expect(url).toContain("https://yandex.by/maps"); 
-    })
-    await addFunctions.goBackToPreviousTab ();
-    await addHomePage.ClickOnMarketButton ();
-    await addFunctions.openNewTab ();
-    await browser.getCurrentUrl().then(function(url) {
-    console.log("URL= "+ url);
-    expect(url).toContain("https://market.yandex.by/"); 
-    })
-    await addFunctions.goBackToPreviousTab ();
-    await addHomePage.ClickOnTranslateButton ();
-    await addFunctions.openNewTab ();
-    await browser.getCurrentUrl().then(function(url) {
-    console.log("URL= "+ url);
-    expect(url).toContain("https://translate.yandex.by/"); 
-    })
-    await addFunctions.goBackToPreviousTab ();
-    await addHomePage.ClickOnMusicButton ();
-    await addFunctions.openNewTab ();
-    await browser.getCurrentUrl().then(function(url) {
-    console.log("URL= "+ url);
-    expect(url).toContain("https://music.yandex.by/"); 
-    })
-    await addFunctions.goBackToPreviousTab ();
-    })
-          afterAll (() =>      {
-      browser.close
-       })
-       
-         });
-         
+let openNewTab = function() {	
+  browser.getAllWindowHandles().then(function(handles) {	
+          let newWindowHandle = handles[1]; 	
+          browser.switchTo().window(newWindowHandle)     });	
+  }
+let goBackToPreviousTab = function() {
+    browser.getAllWindowHandles().then(function (handles) {	
+        browser.driver.switchTo().window(handles[1]);	
+        browser.driver.close();	
+        browser.driver.switchTo().window(handles[0])    });	
+    }
+    describe('Yandex5', () => {
+      it('Click login button', async () => {
+      await browser.get(URL);
+      await addHomePage.ClickOnVideoButton ();
+      openNewTab ();
+      await browser.getCurrentUrl().then(function(url) {
+      console.log("URL= "+ url);
+      expect(url).toContain("https://yandex.by/video"); 
+      })
+      goBackToPreviousTab ();
+
+      await addHomePage.ClickOnImagesButton ();
+      openNewTab ();
+      await browser.getCurrentUrl().then(function(url) {
+      console.log("URL= "+ url);
+      expect(url).toContain("https://yandex.by/images"); 
+      })
+      goBackToPreviousTab ();
+
+      await addHomePage.ClickOnNewsButton ();
+      openNewTab ();
+      await browser.getCurrentUrl().then(function(url) {
+      console.log("URL= "+ url);
+      expect(url).toContain("https://yandex.by/news"); 
+      })
+      goBackToPreviousTab ();
+
+      await addHomePage.ClickOnMapsButton ();
+      openNewTab ();
+      await browser.getCurrentUrl().then(function(url) {
+      console.log("URL= "+ url);
+      expect(url).toContain("https://yandex.by/maps"); 
+      })
+      goBackToPreviousTab ();
+
+      await addHomePage.ClickOnMarketButton ();
+      openNewTab ();
+      await browser.getCurrentUrl().then(function(url) {
+      console.log("URL= "+ url);
+      expect(url).toContain("https://market.yandex.by/"); 
+      })
+      goBackToPreviousTab ();
+      
+      await addHomePage.ClickOnTranslateButton ();
+      openNewTab ();
+      await browser.getCurrentUrl().then(function(url) {
+      console.log("URL= "+ url);
+      expect(url).toContain("https://translate.yandex.by/"); 
+      })
+      goBackToPreviousTab ();
+
+      await addHomePage.ClickOnMusicButton ();
+      openNewTab ();
+      await browser.getCurrentUrl().then(function(url) {
+      console.log("URL= "+ url);
+      expect(url).toContain("https://music.yandex.by/"); 
+      })
+      goBackToPreviousTab ();
+      })
+
+        afterAll (() =>      {
+        browser.close
+         })
+  
+           });
